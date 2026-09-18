3991577f4a542f10d9b3c2defc36f0a34e3e0c5f
gizmore <gizmore@wechall.net>
2026-09-18 04:06:47 +0200

    'more korean'

---
+++ lang/april_ko.php
@@ -0,0 +1,18 @@
+<?php
+namespace GDO\Aprilfools\lang;
+
+return [
+	'cfg_teapot_easteregg' => '찻주전자 이스터에그?',
+	'cfg_april_behaviour' => '만우절의 행동',
+	'err_april_password_taken' => '이 비밀번호는 %s이(가) 이미 사용하고 있습니다!',
+	'err_april_security_wrong' => '보안 질문은 ASCII 대문자 3자를 초과할 수 없습니다.',
+	'err_april_security_invalid' => '유효한 보안 질문을 선택하세요!',
+	'err_april_account_incoherent' => '귀하의 계정에 데이터 손실이 발생했을 수 있습니다!',
+	'err_april_malware' => '귀하의 네트워크는 봇넷으로 인식됩니다. 방법이 허용됩니다!',
+	'err_april_cop' => '당신은 경찰입니다. 우리는 공공 네트워크에서 경찰에게 서비스를 제공하지 않습니다.',
+	'err_april_hacker' => '당신은 해커입니다 :O',
+	'err_april_language' => '무작위로 다른 언어를 선택하십시오.<br/>Ich möchte diesen Teppich nicht kaufen.',
+	'err_april_tea_too_cold' => '차가 차가워요!',
+	'err_april_tea_too_warm' => '차가 너무 뜨거워요!',
+	'msg_april_tea_perfects' => '차의 온도가 완벽하게 유지됩니다!',
+];
